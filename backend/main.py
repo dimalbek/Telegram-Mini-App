f665619b0baa3423b475fd357c8598f0453c5ecd
Dinmukhamed Albek <dinmukhamedalbek@MacBook-Air-Dinmukhamed.local>
2024-10-18 13:34:58 +0500

    bot setup template

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,37 @@
-from fastapi import FastAPI
-import uvicorn
+import logging
+from contextlib import asynccontextmanager
+from backend.bot.create_bot import bot, dp, stop_bot, start_bot
+from backend.bot.handlers.user_router import user_router
+from backend.config import settings
+from aiogram.types import Update
+from fastapi import FastAPI, Request
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.info("Starting bot setup...")
+    dp.include_router(user_router)
+    await start_bot()
+    webhook_url = settings.get_webhook_url()
+    await bot.set_webhook(url=webhook_url,
+                          allowed_updates=dp.resolve_used_update_types(),
+                          drop_pending_updates=True)
+    logging.info(f"Webhook set to {webhook_url}")
+    yield
+    logging.info("Shutting down bot...")
+    await bot.delete_webhook()
+    await stop_bot()
+    logging.info("Webhook deleted")
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5050)
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/webhook")
+async def webhook(request: Request) -> None:
+    logging.info("Received webhook request")
+    update = Update.model_validate(await request.json(), context={"bot": bot})
+    await dp.feed_update(bot, update)
+    logging.info("Update processed")
