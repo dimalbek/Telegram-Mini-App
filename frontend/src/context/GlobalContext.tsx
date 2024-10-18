b717aafd74ccaf495c8129c31bd7d5946af5789e
AlikhanNasa7 <alihannashtaj@gmail.com>
2024-10-18 15:13:58 +0500

    Save local changes before pull

---
+++ frontend/src/context/GlobalContext.tsx
@@ -0,0 +1,66 @@
+import React, {createContext, useState, useMemo} from 'react';
+
+const GlobalContext = createContext<GlobalContextProps | undefined>(undefined);
+
+export interface User {
+    id: number;
+    name: string;
+}
+  
+export interface Course {
+    id: number;
+    title: string;
+}
+
+export interface Notification {
+    id: number;
+    message: string;
+}
+
+export interface GlobalState {
+    user: User | null;
+    courses: Course[];
+    notifications?: Notification[];
+}
+
+export interface GlobalContextProps extends GlobalState {
+    login: (userData: User) => void;
+    logout: () => void;
+    enrollCourse?: (course: Course) => void;
+    unenrollCourse?: (courseId: number) => void;
+    addNotification?: (notification: Notification) => void;
+    removeNotification?: (notificationId: number) => void;
+}
+
+
+export const GlobalProvider = ({ children }: {children: any}) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [courses, setCourses] = useState<Course[]>([]);
+  
+    const login = (userData: any) => {
+      setUser(userData);
+    };
+  
+    const logout = () => {
+      setUser(null);
+    };
+
+    const contextValue = useMemo(
+      () => ({
+        user,
+        courses,
+        login,
+        logout,
+      }),
+      [user, courses]
+    );
+  
+    return (
+      <GlobalContext.Provider value={{user, courses, login, logout}}>
+        {children}
+      </GlobalContext.Provider>
+    );
+};
+
+export default GlobalProvider;
+
---
+++ frontend/src/request.tsx
@@ -0,0 +1,23 @@
+const DEFAULT_URL = "";
+//@ts-ignore
+const TG = window.Telegram.WebApp;
+
+async function getUserData(endpoint: string, method: string = "GET", data? : any){
+    const options = {
+        method,
+        headers: {
+            "Authorization" : TG.initData,
+            "ContentType": "application/json"
+        },
+        body: data ? JSON.stringify(data) : undefined
+    };
+
+    const response = await fetch(endpoint, options);
+    const jsonData = await response.json();
+
+    if (response.ok){
+        return jsonData;
+    }
+}
+
+export {TG, getUserData};
