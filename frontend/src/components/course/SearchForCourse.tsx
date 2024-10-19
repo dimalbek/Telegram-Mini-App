fade6a8a6ce5ad77f8f1f12d1a126bdb959900bf
DeFida <dasiktazik@gmail.com>
2024-10-19 13:52:42 +0500

    test user data

--- frontend/src/components/course/SearchForCourse.tsx
+++ frontend/src/components/course/SearchForCourse.tsx
@@ -3,14 +3,15 @@ import { useEffect, useState } from "react"
 import { Input } from "../ui/input"
 import { TypographyH3, TypographyP } from "../ui/typography"
 import CourseElement from "./CourseElement"
+import { useGlobalContext } from "@/context/GlobalContext"
 
 export const SearchForCourse = () => {
-
+    const {user} = useGlobalContext()
     const [search, setSearch] = useState<string>('')
     const [courses, setCourses] = useState<TCourse[]>([])
 
     useEffect(() => {
-        fetch('https://telegram-mini-app-x496.onrender.com/courses/', 
+        fetch(`https://telegram-mini-app-x496.onrender.com/courses?user_id=${user?.id || '444368298'}`, 
         {
             method: 'GET',
             headers: {
@@ -29,7 +30,7 @@ export const SearchForCourse = () => {
             <TypographyH3 className='text-[24px]'>Courses</TypographyH3>
             <Input type="text" placeholder="Search.." value={search} onChange={(e) => setSearch(e.target.value)} />
             
-            {courses.length ? courses.filter((course) => course.title.toLowerCase().includes(search.toLowerCase())).map(courseData=>{
+            {courses && courses.length ? courses.filter((course) => course.title.toLowerCase().includes(search.toLowerCase())).map(courseData=>{
                 return <CourseElement course={courseData}/>
             })
             :
