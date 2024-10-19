5ff8ce574b7332b636473b1535cc2a50f77c2039
AlikhanNasa7 <alihannashtaj@gmail.com>
2024-10-19 18:39:54 +0500

    123412341234

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 
 import { useGlobalContext } from '@/context/GlobalContext';
 import { useNavigate } from 'react-router';
+import { Avatar } from '@radix-ui/react-avatar';
 
 const Navbar = () => {
   const {user} = useGlobalContext();
@@ -16,7 +17,13 @@ const Navbar = () => {
         <div className="flex items-center space-x-4">
           <span className="text-xl font-bold">{user.tokens_balance}</span>
           <img src="https://cdn-icons-png.flaticon.com/128/10692/10692946.png" alt="" className='w-8 h-8'/>
-          <img src={user?.photo_url || "https://vercel.com/api/www/avatar/kXwUVWYcKQiITwkzB8n8dJFC?s=64"} alt="" className='w-8 h-8'/>
+          <Avatar className="w-8 h-8" onClick={()=>navigate(`users/${user.id}/profile`)}>
+            <img
+              src={user?.photo_url || "https://vercel.com/api/www/avatar/kXwUVWYcKQiITwkzB8n8dJFC?s=64"}
+              alt="Avatar"
+              className="rounded-full object-cover"
+            />
+          </Avatar>
         </div>
       </div>
     </nav>
