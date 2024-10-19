3fd1e653627100a3b471e08e68c9d2e01a628e51
DeFida <dasiktazik@gmail.com>
2024-10-19 19:09:58 +0500

    as

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,20 +10,19 @@ const Navbar = () => {
 
   if (!user) return null;
   return (
-    <nav className="text-white p-3 border-b-4">
+    <nav className="text-white p-3 border-b-2">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="text-xl font-semibold" onClick={()=>navigate('/')}>{`${user && user.first_name}`}</div>
+        <div onClick={()=>navigate('/')}>
+          <p className="text-xl font-semibold">{`${user && user.first_name}`}</p></div>
 
         <div className="flex items-center space-x-4">
-          <span className="text-xl font-bold">{user.tokens_balance}</span>
+          <p className="text-xl font-bold">{user.tokens_balance}</p>
           <img src="https://cdn-icons-png.flaticon.com/128/10692/10692946.png" alt="" className='w-8 h-8'/>
-          <Avatar className="w-8 h-8" onClick={()=>navigate(`users/${user.id}/profile`)}>
-            <img
+          <img
               src={user?.photo_url || "https://vercel.com/api/www/avatar/kXwUVWYcKQiITwkzB8n8dJFC?s=64"}
               alt="Avatar"
-              className="rounded-full object-cover"
+              className="w-8 rounded-full h-8" onClick={()=>navigate(`users/${user.id}/profile`)}
             />
-          </Avatar>
         </div>
       </div>
     </nav>
