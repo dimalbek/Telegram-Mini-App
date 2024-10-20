197bc2bd5150a04a5d7ee6c6081f50b72e630c35
Dinmukhamed Albek <dinmukhamedalbek@MacBook-Air-Dinmukhamed.local>
2024-10-20 23:19:28 +0500

    render ping

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,9 @@ from app.api.lessons import router as lessons_router
 from app.api.quizzes import router as quizzes_router
 from app.api.questions import router as questions_router
 from app.api.user_progress import router as user_progress_router
+import threading
+import time
+import requests
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -27,3 +30,27 @@ app.include_router(modules_router, tags=["modules"])
 app.include_router(lessons_router, tags=["lessons"])
 app.include_router(quizzes_router, tags=["quizzes"])
 app.include_router(questions_router, tags=["questions"])
+
+
+@app.get("/healthcheck")
+def health_check():
+    return {"status": "ok"}
+
+
+# ping
+def send_ping():
+    while True:
+        try:
+            response = requests.get(
+                "https://telegram-mini-app-x496.onrender.com/healthcheck"
+            )
+            print(f"Ping status: {response.status_code}")
+        except Exception as e:
+            print(f"Failed to ping the server: {e}")
+        time.sleep(600)  # Sleep 10 minutes
+
+
+# Run ping thread
+ping_thread = threading.Thread(target=send_ping)
+ping_thread.daemon = True
+ping_thread.start()
